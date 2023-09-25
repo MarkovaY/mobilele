dac4a3d785978f20d551673ccbba2c9a7cef424c
YanaMarkova <yana_markova@yahoo.com>
2023-09-25 16:35:56 +0200

    Initial commit

--- src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
+++ src/main/java/org/softuni/mobilele/model/entity/UserEntity.java
@@ -6,24 +6,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class UserEntity {
+public class UserEntity extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String email;
     private String password;
     private String firstName;
     private String lastName;
     private boolean active;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getEmail() {
         return email;
