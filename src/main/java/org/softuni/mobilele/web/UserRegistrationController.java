04898a31042c50c60be632f921562321faadc5a2
YanaMarkova <yana_markova@yahoo.com>
2023-10-04 16:19:22 +0200

    Initial commit

--- src/main/java/org/softuni/mobilele/web/UserRegistrationController.java
+++ src/main/java/org/softuni/mobilele/web/UserRegistrationController.java
@@ -1,14 +1,34 @@
 package org.softuni.mobilele.web;
 
+import org.softuni.mobilele.model.dto.UserRegistrationDto;
+import org.softuni.mobilele.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/users")
 @Controller
 public class UserRegistrationController {
 
-    @GetMapping("/users/register")
+    private final UserService userService;
+
+    public UserRegistrationController(UserService userService) {
+
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
     public String register() {
         return "auth-register";
+    }
+
+    @PostMapping("/register")
+    public String register(UserRegistrationDto userRegistrationDto) {
+
+        userService.registerUser(userRegistrationDto);
+        return "redirect:/";
     }
 
 }
